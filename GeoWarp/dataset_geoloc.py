5c80e0c5fb7f2cdd83a618b6b5291eba5133854c
Tiziano Gioè <tiziano@MacBook-Pro.station>
2022-12-29 15:29:20 +0100

    feat: changing the way of obtaining gallery and queries

--- GeoWarp/dataset_geoloc.py
+++ GeoWarp/dataset_geoloc.py
@@ -8,6 +8,24 @@ from sklearn.neighbors import NearestNeighbors
 
 import datasets_util
 
+def get__class_id__group_id(utm_east, utm_north, heading, M, alpha, N, L):
+        """Return class_id and group_id for a given point.
+            The class_id is a triplet (tuple) of UTM_east, UTM_north and
+            heading (e.g. (396520, 4983800,120)).
+            The group_id represents the group to which the class belongs
+            (e.g. (0, 1, 0)), and it is between (0, 0, 0) and (N, N, L).
+        """
+        rounded_utm_east = int(utm_east // M * M)  # Rounded to nearest lower multiple of M
+        rounded_utm_north = int(utm_north // M * M)
+        rounded_heading = int(heading // alpha * alpha)
+        
+        class_id = (rounded_utm_east, rounded_utm_north, rounded_heading)
+        # group_id goes from (0, 0, 0) to (N, N, L)
+        group_id = (rounded_utm_east % (M * N) // M,
+                    rounded_utm_north % (M * N) // M,
+                    rounded_heading % (alpha * L) // alpha)
+        return class_id, group_id
+
 
 class GeolocDataset(data.Dataset):
     def __init__(self, dataset_folder="/content/small", dataset_name="sf-xs", split="train", positive_dist_threshold=25):
@@ -20,8 +38,27 @@ class GeolocDataset(data.Dataset):
         
         if split == "train":
             #### Read paths and UTM coordinates for all images.
-            self.database_paths = sorted(glob(join(self.dataset_folder, "37.7*", "*.jpg"), recursive=True))
-            self.queries_paths = sorted(glob(join(self.dataset_folder, "37.8*", "*.jpg"), recursive=True))
+            gallery_proportion = 0.8
+            queries_prop = 1 - gallery_proportion
+            images_per_class = 10
+
+            images_paths = sorted(glob(self.dataset_folder, "/**/*.jpg"), recursive=True)
+            class_id_path = {}
+            for path in images_paths:
+                utm_east = float(path.split("@")[1])
+                utm_north = float(path.split("@")[2])
+                heading = float(path.split("@")[9])
+                class_id, _ = get__class_id__group_id(utm_east, utm_north, heading, M=10, alpha=30, N=5, L=2)
+                if class_id not in class_id_path:
+                    class_id_path[class_id] = []
+                class_id_path[class_id].append(path)
+
+            self.database_paths = []
+            self.queries_paths = []
+
+            for class_id, paths in class_id_path.items():
+                self.database_paths += paths[:int(images_per_class * gallery_proportion)] # 0 to 7
+                self.queries_paths += paths[int(images_per_class * gallery_proportion):] # 8 to 9
             
             # The format must be path/to/file/@utm_easting@utm_northing@...@.jpg
             self.database_utms = np.array([(path.split("@")[1], path.split("@")[2]) for path in self.database_paths]).astype(np.float)

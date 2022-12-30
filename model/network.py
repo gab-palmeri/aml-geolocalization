a6f160901e3e9290992829c609ea7c8d586c65f1
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2022-12-30 12:59:21 +0100

    test alternative pretrained

--- model/network.py
+++ model/network.py
@@ -4,8 +4,13 @@ import logging
 import torchvision
 from torch import nn
 
-from Team.model.layers import Flatten, L2Norm, GeM, ParametrizedGradientReversalLayer
+from Team.model.layers import Flatten, L2Norm, GeM
 
+# Pretrained models on Google Landmarks v2 and Places 365
+PRETRAINED_MODELS = {
+        'resnet18_places'  : '1DnEQXhmPxtBUrRc81nAvT8z17bk-GBj5',
+        'resnet18_gldv2'   : '1wkUeUXFXuPHuEvGTXVpuP5BMB-JJ1xke',
+    }
 
 CHANNELS_NUM_IN_LAST_CONV = {
         "resnet18": 512,
@@ -13,15 +18,13 @@ CHANNELS_NUM_IN_LAST_CONV = {
         "resnet101": 2048,
         "resnet152": 2048,
         "vgg16": 512,
-        "convnext-tiny": 768,
-        "efficientnetv2-s": 256,
     }
 
 
 class GeoLocalizationNet(nn.Module):
-    def __init__(self, backbone, fc_output_dim):
+    def __init__(self, backbone, fc_output_dim, pretrain):
         super().__init__()
-        self.backbone, features_dim = get_backbone(backbone)
+        self.backbone, features_dim = get_backbone(backbone, pretrain)
         self.aggregation = nn.Sequential(
                 L2Norm(),
                 GeM(),
@@ -35,32 +38,13 @@ class GeoLocalizationNet(nn.Module):
         x = self.aggregation(x)
         return x
 
-class GeoAdapt(nn.Module):
-    def __init__(self, backbone, fc_output_dim):
-        super().__init__()
-        # backbone
-        self.backbone, features_dim = get_backbone(backbone)
-        # gradient reversal layer
-        self.grl = ParametrizedGradientReversalLayer(0.5)
-        # GeoLocalizationNet layer
-        self.gln = nn.Sequential(
-                L2Norm(),
-                GeM(),
-                Flatten(),
-                nn.Linear(features_dim, fc_output_dim),
-                L2Norm()
-            )
-    
-    def forward(self, x):
-        x = self.backbone(x)
-        x = self.grl(x)
-        x = self.gln(x)
-        return x
-
-def get_backbone(backbone_name):
+def get_backbone(backbone_name, pretrain):
     if backbone_name.startswith("resnet"):
         if backbone_name == "resnet18":
-            backbone = torchvision.models.resnet18(pretrained=True)
+            if pretrain in [x.split("_")[-1] for x in PRETRAINED_MODELS.keys()]:
+                backbone = get_pretrained_resnet18(backbone_name, pretrain)
+            else:
+                backbone = torchvision.models.resnet18(pretrained=True)
         elif backbone_name == "resnet50":
             backbone = torchvision.models.resnet50(pretrained=True)
         elif backbone_name == "resnet101":
@@ -84,24 +68,34 @@ def get_backbone(backbone_name):
                 p.requires_grad = False
         logging.debug("Train last layers of the VGG-16, freeze the previous ones")
     
-    elif backbone_name == "convnext-tiny":
-        backbone = torchvision.models.convnext_tiny(pretrained=True)
-        layers = list(backbone.features.children()) # Remove avg pooling and FC layer
-        for layer in layers[:-1]:
-            for p in layer.parameters():
-                p.requires_grad = False
-        logging.debug("Train last 3 CNBlocks of the ConvNext-Tiny, freeze the previous ones")
+    else:
+        raise ValueError(f"Backbone {backbone_name} is not supported")
 
-    elif backbone_name == "efficientnetv2-s":
-        backbone = torchvision.models.efficientnet_v2_s(pretrained=True)
-        layers = list(backbone.features.children())[:-1] # Remove avg pooling and FC layer
-        for layer in layers[:-1]:
-            for p in layer.parameters():
-                p.requires_grad = False
-        logging.debug("Train last sequential of EfficientNetV2-S, freeze the previous ones")
-    
     backbone = torch.nn.Sequential(*layers)
     
     features_dim = CHANNELS_NUM_IN_LAST_CONV[backbone_name]
     
     return backbone, features_dim
+
+def get_pretrained_resnet18(backbone_name, pretrain):
+    import os
+    import GoogleDriveDownloader as gdd
+
+    if pretrain == 'places':  num_classes = 365
+    elif pretrain == 'gldv2':  num_classes = 512
+    
+    if backbone_name.startswith("resnet18"):
+        model = torchvision.models.resnet18(num_classes=num_classes)
+    else:
+        raise ValueError("This method supports only resnet18, backbone is: {}".format(backbone_name))
+    
+    model_name = backbone_name.split('conv')[0] + "_" + pretrain
+    file_path =  f"content/pretrained/{model_name}.pth"
+    
+
+    if not os.path.exists(file_path):
+        gdd.download_file_from_google_drive(file_id=PRETRAINED_MODELS[model_name],
+                                            dest_path=file_path)
+    state_dict = torch.load(file_path, map_location=torch.device('cpu'))
+    model.load_state_dict(state_dict)
+    return model
